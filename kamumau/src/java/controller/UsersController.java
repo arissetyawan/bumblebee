4c955d97dc188b69213f4432319da93f474d29a1
Mohammad Syafiq Marzuki <37768863+syafiqmarzuki@users.noreply.github.com>
2018-10-17 14:08:43 +0700

    Add files via upload

--- kamumau/src/java/controller/UsersController.java
+++ kamumau/src/java/controller/UsersController.java
@@ -7,11 +7,14 @@ package controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.User;
 
 /**
  *
@@ -19,6 +22,11 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet(name = "UsersController", urlPatterns = {"/UsersController"})
 public class UsersController extends HttpServlet {
+    private final static String add_action ="new";
+    private final static String login_action = "login";
+    private final static String logout_action = "logout";
+    private final static String list_action = "list";
+    private String message="";
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -34,15 +42,33 @@ public class UsersController extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet UsersController</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet UsersController at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+            String action = request.getParameter("action");
+            if(action==null){
+                action= "list";
+            }
+            try{
+                switch (action){
+                    case "new":
+                        ShowNewForm(request, response);
+                        break;
+                    case "create":
+                        createUser(request, response);
+                        break;
+                    case "login":
+                        doLogin(request, response);
+                        break;
+                    case "logout":
+                        doLogout(request, response);
+                    case "welcomepage":
+                        showWelcomePage(request, response);
+                    default:
+                        showLoginForm(request, response);
+                        break;
+                }
+            }
+            catch (SQLException ex){
+                throw new ServletException(ex);
+            }
         }
     }
 
@@ -84,5 +110,81 @@ public class UsersController extends HttpServlet {
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
+
+    private void doLogin(HttpServletRequest request, HttpServletResponse response) 
+    throws SQLException, IOException,ServletException{
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        User user= new User();
+        user.setEmail(email);
+        user.setPassword(password);
+        logout(request, response);
+        if(user.doLogin()){
+            setLoggedIn(user, request, response);
+            showWelcomePage(request, response);
+        }
+        
+    }
+
+    private void doLogout(HttpServletRequest request, HttpServletResponse response) 
+    throws ServletException,IOException,SQLException{
+        logout(request, response);
+        showLoginForm(request, response);
+    }
+
+    private void showLoginForm(HttpServletRequest request, HttpServletResponse response) 
+    throws ServletException,IOException{
+        RequestDispatcher dispatcher = request.getRequestDispatcher("users/login.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    private void showWelcomePage(HttpServletRequest request, HttpServletResponse response) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void logout(HttpServletRequest request, HttpServletResponse response) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void setLoggedIn(User user, HttpServletRequest request, HttpServletResponse response) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void ShowNewForm(HttpServletRequest request, HttpServletResponse response) 
+    throws ServletException,IOException{
+        RequestDispatcher dispatcher = request.getRequestDispatcher("users/signup.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    private void createUser(HttpServletRequest request, HttpServletResponse response) 
+    throws SQLException,IOException,ServletException{
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        String retpassword = request.getParameter("retpassword");
+        String fullname = request.getParameter("fullname");
+        String address = request.getParameter("address");
+        String bankname = request.getParameter("bankname");
+        String accountno = request.getParameter("accountno");
+        
+        User user = new User();
+        user.setEmail(email);
+        user.setPassword(password);
+        user.setRetpassword(retpassword);
+        user.setFullname(fullname);
+        user.setAddress(address);
+        user.setBankname(bankname);
+        user.setAccountno(accountno);
+        if(user.create()){
+            message="new user added";
+            request.setAttribute("message", message);
+            response.sendRedirect("user?action="+add_action);
+        }
+        else{
+            message= "new user failed to add";
+            request.setAttribute("message", message);
+            request.getRequestDispatcher("user?action="+add_action).include(request, response);
+        }
+        
+    }
 
 }
