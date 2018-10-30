131f718f046fdee284f96391252821a12ec37dec
Mohammad Syafiq Marzuki <37768863+syafiqmarzuki@users.noreply.github.com>
2018-10-30 20:24:45 +0700

    Add files via upload

--- kamumau/src/java/model/User.java
+++ kamumau/src/java/model/User.java
@@ -4,18 +4,18 @@
  * and open the template in the editor.
  */
 package model;
-
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Date;
 
 /**
  *
  * @author x201
  */
-public class User extends MyConnection{
-    private final String tableName="user";
+public class User extends MyConnection {
+    private final String tableName ="user";
     public String email;
     public String password;
     public String retpassword;
@@ -23,6 +23,8 @@ public class User extends MyConnection{
     public String address;
     public String bankname;
     public String accountno;
+    public Date created_at;
+    public Date updated_at;
 
     public void setEmail(String email) {
         this.email = email;
@@ -52,23 +54,19 @@ public class User extends MyConnection{
         this.accountno = accountno;
     }
 
-   
-    public boolean doLogin(){
-        this.id=1;
-        return true;
+    public void setCreated_at(Date created_at) {
+        this.created_at = created_at;
+    }
+
+    public void setUpdated_at(Date updated_at) {
+        this.updated_at = updated_at;
     }
     
-    public int getId(){
-        return this.id;
+    private String generateDate(){
+        Date date= new Date();
+        return String.format("%1$tY-%1$tm-%1$td", date);
     }
     
-    public void setId(int id){
-        this.id=id;
-    }
-    
-    public String getName(){
-        return "Nama User";
-    }
 
     public String getTableName() {
         return tableName;
@@ -102,12 +100,26 @@ public class User extends MyConnection{
         return accountno;
     }
     
+    public int getId(){
+        /// return id
+        return this.id;
+    }
+    
+    public void setId(int id){
+        this.id=id;
+    }
+
+    public String getName(){
+        return "Nama user";
+    }
+    
     public boolean create(){
+        String now_date= generateDate();
         boolean result;
         if(!validate()){
             return false;
         }
-        String query ="INSERT INTO "+ tableName+"(email,password,fullname,address,bankname,accountno) values ('"+this.email+"','"+ this.password+"','"+ this.fullname+"','"+ this.address+"','"+ this.bankname+"','"+ this.accountno+"')";
+        String query ="INSERT INTO "+ tableName+"(email,password,fullname,address,bankname,accountno,created_at) values ('"+this.email+"','"+ this.password+"','"+ this.fullname+"','"+ this.address+"','"+ this.bankname+"','"+ this.accountno+"','"+now_date+"')";
         try{
             Statement stmt = this.conn().createStatement();
             return stmt.executeUpdate(query)>0;
@@ -116,7 +128,7 @@ public class User extends MyConnection{
             return false;
         }
     }
-
+    
     private boolean validate() {
         boolean status = false;
         if(!"".equals(this.email)&& !"".equals(this.password)&&!"".equals(this.retpassword)&& !"".equals(this.fullname)&& !"".equals(this.fullname)&& !"".equals(this.address)&& !"".equals(this.bankname)&& !"".equals(this.accountno)){
@@ -139,6 +151,7 @@ public class User extends MyConnection{
                 user.setAddress(res.getString("address"));
                 user.setBankname(res.getString("bankname"));
                 user.setAccountno(res.getString("accountno"));
+                user.setCreated_at(res.getDate("created_at"));
                 
                 user.setId(res.getInt("id"));
                 users.add(user);
@@ -149,42 +162,63 @@ public class User extends MyConnection{
         return users;
     }
     
-    public User find(int id){
+//    public User find(int id){
+//        User user = new User();
+//        String query = "SELECT * FROM " + tableName + " WHERE id = " + id + " ";
+//        try {
+//            Statement stmt = this.conn().createStatement();
+//            ResultSet res = stmt.executeQuery(query);
+//            if (res.next()) {
+//                user.setEmail(res.getString("name"));
+//                user.setPassword(res.getString("phone"));
+//                user.setRetpassword(res.getString("profession"));
+//                user.setFullname (res.getString("fullname"));
+//                user.setAddress(res.getString("address"));
+//                user.setBankname(res.getString("bankname"));
+//                user.setAccountno(res.getString("accountno"));
+//                user.setCreated_at(res.getDate("created_at"));
+//                user.setId(res.getInt("id"));
+//            }
+//        } catch (SQLException e) {
+//            System.out.println(e.getMessage());
+//        }
+//        return user;
+//    }
+
+    public boolean doLogin() {
         User user = new User();
-        String query = "SELECT * FROM " + tableName + " WHERE id = " + id + " ";
+        // find to db match it by email and password;
+        String query = "SELECT * FROM " + tableName + " WHERE email = '" + this.email + "' and password = '" + this.password + "'";
+        // select * from user where email = this.email and password= this.password
         try {
             Statement stmt = this.conn().createStatement();
             ResultSet res = stmt.executeQuery(query);
             if (res.next()) {
-                user.setEmail(res.getString("name"));
-                user.setPassword(res.getString("phone"));
-                user.setRetpassword(res.getString("profession"));
-                user.setFullname (res.getString("fullname"));
-                user.setAddress(res.getString("address"));
-                user.setBankname(res.getString("bankname"));
-                user.setAccountno(res.getString("accountno"));
+                user.setEmail(res.getString("email"));
+                user.setPassword(res.getString("password"));
                 user.setId(res.getInt("id"));
+                System.out.println("id nya"+ this.id);
+                System.out.println("nya"+ res.getInt("id"));
+
+                this.id= res.getInt("id"); //get it from db
+                return true;
+            }else{
+                return false;
             }
         } catch (SQLException e) {
             System.out.println(e.getMessage());
+            return false;
         }
-        return user;
     }
     
-//    public boolean login(){
-//        User user = new User();
-//        String query ="SELECT * FROM " + tableName + " WHERE email = " + email + " and password= "+ password+ ";
-//        try{
-//            Statement stmt = this.conn().createStatement();
-//            ResultSet res= stmt.executeQuery(query);
-//        }catch(SQLException e){
-//            System.out.println(e.getMessage());
-//            return false;
-//        }
-//        return false;
-//    }
-//    
     
+    
+    
+    
+
+
+
+
     
     
 }
